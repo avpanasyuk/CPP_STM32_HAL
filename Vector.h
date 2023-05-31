23adc27dce26ae61cf4f78fc81e60276d6592927
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2023-05-31 18:56:21 -0400

    ARM CMSIS library has vector functions, can write a class

--- Vector.h
+++ Vector.h
@@ -1,1 +1,18 @@
- 
+#pragma once
+
+/// @cond
+#include <array>
+/// @endcond
+
+namespace avp {
+  template <typename T, std::size_t N>
+  class ARM_Vector : public std::array<T, N> {
+  public:
+    typedef std::array<T, N> array_t;
+
+    ARM_Vector() {}
+    ARM_Vector(const std::initializer_list<T> &t): array_t(t) {}
+    ARM_Vector(const T &rhs) : Vector<T, N>(rhs) {}
+  }; // class ARM_Vector
+} // namespace avp
+
